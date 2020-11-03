cdcf19d167cd0ec6437bccbc2df535c26246a2db
andreaives <andreasue3@gmail.com>
2020-11-02 20:55:47 -0700

    connection and controller

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -11,7 +11,7 @@ $(function() {
    // Send the PUT request.
    $.ajax("/api/burgers/" + id, {
      type: "PUT",
-     data: newSleep
+     data: newBurger
    }).then(
      function() {
        console.log("changed Burger to", newBurger);
